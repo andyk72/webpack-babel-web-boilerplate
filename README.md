688ded9f31e1e403dbdc36251143c5267e21559a
Andrea Padovani <a.padovani@amtek.site>
2020-02-04 00:34:59 +0100

    feat: webpack + babel (ES6) + html-webpack-plugin

---
+++ README.md
@@ -0,0 +1,22 @@
+## Features
+
+1. Javascript bundling (Webpack)
+2. Javascript ES6 support (Webpack + Babel loader)
+3. Index.html template (Webpack with html-webpack-plugin)
+
+## Resources
+
+babel
+-----
+
+https://babeljs.io/
+
+webpack
+-------
+
+https://webpack.js.org/
+
+html-webpack-plugin
+-------------------
+
+https://github.com/jantimon/html-webpack-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: "./src/js/index.js",
     output: {
-        path: path.resolve(__dirname, "public/js"),
-        filename: "bundle.js"
-    }
+        path: path.resolve(__dirname, "public"),
+        filename: "js/bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({  // Also generate a test.html
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: 'Boilerplate for HTML5/Javascript web project with Webpack and Babel ES6 support',
+        })
+    ]
 };
